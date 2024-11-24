c69bab57788c50c3b7716a0a9aaa0274c70f237c
Darun Karas <darunkaras@Daruns-MacBook-Air.local>
2024-11-24 20:31:43 +0600

    server is created

---
+++ index.js
@@ -0,0 +1,14 @@
+const express = require('express');
+const cors = require('cors');
+const port = process.env.PORT || 5001;
+const app = express();
+
+//middleware 
+app.use(cors());
+app.use(express.json());
+app.get('/', (req, res) => {
+    res.send("Crud server is running");
+})
+app.listen(port, () => {
+    console.log(`Server is running on port ${port}`);
+})

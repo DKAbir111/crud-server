df0fc6ab9a593d0966190687f81fa69676cd2bf4
Darun Karas <darunkaras@Daruns-MacBook-Air.local>
2024-11-25 10:55:36 +0600

    data added to mongodb

--- index.js
+++ index.js
@@ -8,7 +8,6 @@ const app = express();
 app.use(cors());
 app.use(express.json());
 
-
 const uri = "mongodb+srv://dkabir:YNPXS7VmaCYkKrnz@cluster0.xratx.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0";
 
 // Create a MongoClient with a MongoClientOptions object to set the Stable API version
@@ -24,20 +23,20 @@ async function run() {
     try {
         // Connect the client to the server	(optional starting in v4.7)
         await client.connect();
-        app.post('/users', (req, res) => {
+        const database = client.db('userDB');
+        const usersCollection = database.collection('users');
+        app.post('/users', async (req, res) => {
             const user = req.body;
-            console.log(user);
-            res.send(user);
+            const result = await usersCollection.insertOne(user);
+            res.send(result);
         })
-
-
 
         // Send a ping to confirm a successful connection
         await client.db("admin").command({ ping: 1 });
         console.log("Pinged your deployment. You successfully connected to MongoDB!");
     } finally {
         // Ensures that the client will close when you finish/error
-        await client.close();
+        // await client.close();
     }
 }
 run().catch(console.dir);
